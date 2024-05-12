7375cd4d187cb379781804f564ba40bcb64d66bc
fadlurahmanf <tffajari@gmail.com>
2024-05-12 17:57:24 +0700

    fix(ci cd): change version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         minSdk = 21
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.0.1"
 
         buildConfigField("String", "EQURAN_BASE_URL", "\"https://equran.id/\"")
 
