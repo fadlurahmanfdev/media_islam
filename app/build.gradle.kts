721475070461c6eb361aaa27dc83e967f116faa1
fadlurahmanf <tffajari@gmail.com>
2024-05-12 16:51:12 +0700

    fix(ci cd): fixing properties in build gradle related debug & release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,9 +9,13 @@ plugins {
     id("com.google.gms.google-services")
 }
 
-val keystorePropertiesFile = rootProject.file("signing.properties")
-val keystoreProperties = Properties()
-keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+val debugPropertiesFile = rootProject.file("signingDebug.properties")
+val debugProperties = Properties()
+debugProperties.load(FileInputStream(debugPropertiesFile))
+
+val releasePropertiesFile = rootProject.file("signingRelease.properties")
+val releaseProperties = Properties()
+releaseProperties.load(FileInputStream(releasePropertiesFile))
 
 android {
     namespace = "co.id.fadlurahmanf.mediaislam"
@@ -31,16 +35,16 @@ android {
 
     signingConfigs {
         getByName("debug") {
-            storeFile = file("debug.keystore")
-            keyAlias = "androiddebugkey"
-            keyPassword = "android"
-            storePassword = "android"
+            keyAlias = debugProperties["keyAlias"] as String
+            keyPassword = debugProperties["keyPassword"] as String
+            storeFile = file(debugProperties["storeFilePath"] as String)
+            storePassword = debugProperties["storePassword"] as String
         }
         create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = file(keystoreProperties["storeFilePath"] as String)
-            storePassword = keystoreProperties["storePassword"] as String
+            keyAlias = releaseProperties["keyAlias"] as String
+            keyPassword = releaseProperties["keyPassword"] as String
+            storeFile = file(releaseProperties["storeFilePath"] as String)
+            storePassword = releaseProperties["storePassword"] as String
         }
     }
 
