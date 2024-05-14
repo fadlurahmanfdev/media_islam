50c1b234382180517ca5999c5635410472ec8f6b
fadlurahmanf <tffajari@gmail.com>
2024-05-14 07:19:00 +0700

    feat(ci cd): change build gradle kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "co.id.fadlurahmanf.mediaislam"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.0.1 ($versionCode)"
 
         buildConfigField("String", "EQURAN_BASE_URL", "\"https://equran.id/\"")
 
