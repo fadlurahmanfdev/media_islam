0aa9fd34d35d67c9e8b5675c52149db813d5c9c0
fadlurahmanf <tffajari@gmail.com>
2024-07-27 10:20:53 +0700

    feat(alarm): set alarm

--- app/src/main/java/co/id/fadlurahmanf/mediaislam/main/presentation/alarm/AlarmActivity.kt
+++ app/src/main/java/co/id/fadlurahmanf/mediaislam/main/presentation/alarm/AlarmActivity.kt
@@ -1,6 +1,10 @@
 package co.id.fadlurahmanf.mediaislam.main.presentation.alarm
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
 import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.annotation.DrawableRes
@@ -10,6 +14,8 @@ import co.id.fadlurahmanf.mediaislam.core.state.AladhanNetworkState
 import co.id.fadlurahmanf.mediaislam.databinding.ActivityAlarmBinding
 import co.id.fadlurahmanf.mediaislam.main.BaseMainActivity
 import co.id.fadlurahmanf.mediaislam.main.data.dto.model.PrayersTimeModel
+import co.id.fadlurahmanf.mediaislam.main.domain.receiver.AlarmReceiver
+import java.util.Calendar
 import javax.inject.Inject
 
 class AlarmActivity : BaseMainActivity<ActivityAlarmBinding>(ActivityAlarmBinding::inflate) {
@@ -32,6 +38,7 @@ class AlarmActivity : BaseMainActivity<ActivityAlarmBinding>(ActivityAlarmBindin
         binding.toolbar.tvTitle.visibility = View.VISIBLE
 
         initObserver()
+        initAction()
 
         viewModel.getTodayPrayerTime()
     }
@@ -58,6 +65,42 @@ class AlarmActivity : BaseMainActivity<ActivityAlarmBinding>(ActivityAlarmBindin
         }
     }
 
+    private lateinit var alarmManager: AlarmManager
+
+    private fun initAction() {
+        alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        binding.itemDhuhr.switchAlarm.setOnCheckedChangeListener { _, checked ->
+            println("MASUK_ CHECKED: $checked")
+            if (checked) {
+                scheduleAlarm()
+            }
+        }
+    }
+
+    private fun scheduleAlarm() {
+        val calendar = Calendar.getInstance().apply {
+            add(Calendar.SECOND, 10)
+        }
+
+        val pendingIntent = AlarmReceiver.getPendingIntent(this)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                println("MASUK_ CAN SCHEDULE EXACT ALARM: ${alarmManager.canScheduleExactAlarms()}")
+                if (alarmManager.canScheduleExactAlarms()) {
+                    alarmManager.setExact(
+                        AlarmManager.RTC, calendar.timeInMillis, pendingIntent
+                    )
+                }
+            } else {
+                println("MASUK_ SCHEDULE EXACT ALARM BELOW API 31")
+                alarmManager.setExact(
+                    AlarmManager.RTC, calendar.timeInMillis, pendingIntent
+                )
+            }
+
+        }
+    }
+
     private fun setPrayerTime(data: PrayersTimeModel) {
         binding.itemFajr.ivIconPrayer.setImageDrawable(
             ContextCompat.getDrawable(
@@ -65,6 +108,7 @@ class AlarmActivity : BaseMainActivity<ActivityAlarmBinding>(ActivityAlarmBindin
                 R.drawable.fajr
             )
         )
+        binding.itemFajr.tvPrayerTimeType.text = "Subuh"
         binding.itemFajr.tvPrayerTime.text = data.timing.fajr
 
         binding.itemDhuhr.ivIconPrayer.setImageDrawable(
@@ -73,6 +117,7 @@ class AlarmActivity : BaseMainActivity<ActivityAlarmBinding>(ActivityAlarmBindin
                 R.mipmap.dhuhr
             )
         )
+        binding.itemDhuhr.tvPrayerTimeType.text = "Zuhur"
         binding.itemDhuhr.tvPrayerTime.text = data.timing.dhuhr
 
         binding.itemAsr.ivIconPrayer.setImageDrawable(
@@ -81,6 +126,7 @@ class AlarmActivity : BaseMainActivity<ActivityAlarmBinding>(ActivityAlarmBindin
                 R.mipmap.asr
             )
         )
+        binding.itemAsr.tvPrayerTimeType.text = "Ashar"
         binding.itemAsr.tvPrayerTime.text = data.timing.asr
 
         binding.itemMaghrib.ivIconPrayer.setImageDrawable(
@@ -89,6 +135,7 @@ class AlarmActivity : BaseMainActivity<ActivityAlarmBinding>(ActivityAlarmBindin
                 R.drawable.maghrib
             )
         )
+        binding.itemMaghrib.tvPrayerTimeType.text = "Maghrib"
         binding.itemMaghrib.tvPrayerTime.text = data.timing.maghrib
 
         binding.itemIsha.ivIconPrayer.setImageDrawable(
@@ -97,6 +144,7 @@ class AlarmActivity : BaseMainActivity<ActivityAlarmBinding>(ActivityAlarmBindin
                 R.drawable.isha
             )
         )
+        binding.itemIsha.tvPrayerTimeType.text = "Isya"
         binding.itemIsha.tvPrayerTime.text = data.timing.isha
     }
 
