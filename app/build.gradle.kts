0d3fd4fe81d60e00bec5555ba460a8f1f3acecfa
fadlurahmanfdev <taufikfadlurahmanfajari.dev@gmail.com>
2024-11-27 13:33:28 +0700

    remove landing activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         applicationId = "co.id.fadlurahmanf.mediaislam"
         minSdk = 21
         targetSdk = 34
-        versionCode = 9
+        versionCode = 10
         versionName = "0.0.1"
 
         buildConfigField("String", "EQURAN_BASE_URL", "\"https://equran.id/api/\"")
